e9382bf330b0513c47ad0341492883c5d52544f3
Malcolm Childs <michilds0@gmail.com>
2021-09-23 00:16:29 -0400

    - Create new lambda function for sending invite via text message - Move lambda dependencies to lambda layers

---
+++ amplify/backend/function/sendInviteLambda/src/index.js
@@ -0,0 +1,41 @@
+const AWSXRay = require('aws-xray-sdk-core');
+const { SSMClient, GetParametersByPathCommand } = AWSXRay.captureAWS(require('aws-sdk/client-ssm'));
+const region = process.env.REGION;
+const ssm = new SSMClient({ region });
+const systemPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const input = {
+    MaxResults: 2,
+    Path: process.env.TWILIO_CREDS_PATH,
+    WithDecryption: true
+};
+const command = new GetParametersByPathCommand(input);
+
+exports.handler = async (event) => {
+    const { inviteePhoneNumber } = JSON.parse(event.body);
+
+    try {
+        const { Parameters } = await ssm.send(command);
+        const [twilioAccountId, twilioAuthToken] = Parameters;
+        const twilio = require('twilio')(twilioAccountId, twilioAuthToken, { lazyLoading: true });
+        await twilio.messages.create({
+            body: 'Hello from lambda',
+            to: inviteePhoneNumber,
+            from: systemPhoneNumber
+        })
+    
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            }, 
+            body: JSON.stringify(`Invite message sent successfully to ${inviteePhoneNumber}`),
+        };        
+    } catch (error) {
+        console.error("Error sending invite. ", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal server error." })
+        }
+    }
+};
